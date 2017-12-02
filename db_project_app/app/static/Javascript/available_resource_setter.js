44ff79c737d2bf8c0ead4ed59caf64d6465d9f02
gretchypr <gretchen.bonilla@upr.edu>
2017-12-02 10:12:45 -0400

    Commit 12
    Can now show JSON in HTML template.

--- db_project_app/app/static/Javascript/available_resource_setter.js
+++ db_project_app/app/static/Javascript/available_resource_setter.js
@@ -1,16 +1,37 @@
 // When the document has finished loading:
   $(document).ready(function(){
     $.getJSON( "available/all", function( data ) {
-      alert("I actually got here.")
       var resources = [];
       $.each( data, function( key, val ) {
-        resources.push( "<li id='" + key + "'>"
-        + val + "</li>" );
+          console.log(key)
+        $.each( val, function( index, val ) {
+          console.log(index, val)
+          resources.push( "<div id='res'>"
+          + "<h2>"+ val.rname + "</h2>"
+          + "<div id=rid>"
+          + "<label><b>Resource ID: <b>"+ val.rid + "</label>"
+          + "</div> <div id=rquantity>"
+          + "<label><b>Quantity: <b>"+ val.rquantity + "</label>"
+          + "</div><div id=rprice>"
+          + "<label><b>Price: <b>"+ val.rprice + "</label>"
+          + "</div></div>" );
+        });
       });
 
       $( "<ul/>", {
         "class": "my-new-list",
         html: resources.join( "" )
       }).appendTo( ".resources" );
+      $(res).css({
+        "padding":"5px",
+        "padding-left": "10px",
+        "width": "90%",
+        "height": "10%",
+        "background-color": "	#F0F8FF",
+        "margin": "auto, 5px",
+        "margin-top" : "10px",
+        "border": "2px solid #89CFF0"
+      });
+    
     });
 });
