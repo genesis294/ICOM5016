2997350f01a9c969ace6b432f2f56ca889e09254
WilfredoRuiz <33869167+WilfredoRuiz@users.noreply.github.com>
2017-12-04 16:51:57 -0400

    Add files via upload

--- db_project_app/app/views.py
+++ db_project_app/app/views.py
@@ -5,18 +5,17 @@ from app import app
 
 # Route for home page
 from handler.transactions import TransactionsHandler
-
+from handler.users import UsersHandler
 
 @app.route('/')
 def home():
     return render_template('DisasterSite.html')
 
-
 # Route for available resources page
 @app.route('/available')
 def available_res():
     if not request.args:
-        return render_template('resource_list.html', resource_type="available")
+        return render_template("available_resources.html")
     else:
         return ResourcesHandler().search_for_available(request.args)
 
@@ -33,25 +32,16 @@ def get_available_by_id(rid):
 
 @app.route('/requested')
 def requested_res():
+    #return render_template('requested_resources.html')
     if not request.args:
-        return render_template('resource_list.html', resource_type="requested")
+        return ResourcesHandler().get_requested_resources()
     else:
         return ResourcesHandler().search_for_request(request.args)
-
-
-@app.route('/requested/all')
-def get_all_req_resources():
-    return ResourcesHandler().get_requested_resources()
 
 
 @app.route('/requested/<int:rid>')
 def get_request_by_id(rid):
     return ResourcesHandler().get_request_by_id(rid)
-
-
-@app.route('/resource_profile')
-def resource_profile():
-    return render_template('resource_profile.html')
 
 
 @app.route('/add')
@@ -109,3 +99,30 @@ def getTransactionsByUserID(uid):
 @app.route('/transactions/cart/<int:cid>')
 def getTransactionsByCartID(cid):
     return TransactionsHandler().getTransactionsByCartID(cid)
+
+##############################
+# Routes to search for Users #
+##############################
+
+@app.route('/users')
+def getAllUsers():
+    if not request.args:
+        return UsersHandler().getAllUsers()
+    else:
+        return UsersHandler().searchUsers(request.args)
+
+@app.route('/users/<int:uid>')
+def getUsersById(uid):
+    return UsersHandler().getUsersById(uid)
+
+@app.route('/users/fname/<string:fname>')
+def getUsersByfName(fname):
+    return UsersHandler().getUsersByfName(fname)
+
+@app.route('/users/lname/<string:lname>')
+def getUsersBylName(lname):
+    return UsersHandler().getUsersBylName(lname)
+
+@app.route('/users/fname/<string:fname>/lname/<string:lname>')
+def getUsersByfNameAndlName(fname, lname):
+    return UsersHandler().getUsersByfNameAndlName(fname,lname)
