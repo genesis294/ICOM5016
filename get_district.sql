125a7f0cd4c95136eb582bf75f5d4b6ea5e65714
gretchypr <gretchen.bonilla@upr.edu>
2017-12-28 22:03:31 -0400

    Commit 26

---
+++ get_district.sql
@@ -0,0 +1,28 @@
+﻿create or replace function get_district (city varchar) 	returns varchar as $$
+begin
+	if (city = 'San Juan' or city = 'Aguas Buenas' or city = 'Guaynabo') then 
+		return 'San Juan';
+	elseif (city = 'Bayamon' or city = 'Catano' or city = 'Toa Alta' or city = 'Toa Baja') then
+		return 'Bayamon';
+	elseif (city = 'Arecibo' or city = 'Barceloneta' or city = 'Camuy' or city = 'Ciales' or city = 'Dorado' or city = 'Florida' or 
+		city = 'Hatillo' or city = 'Manati' or city = 'Morovis' or city = 'Quebradillas' or city = 'Vega Alta' or city = 'Vega Baja') then
+		return 'Arecibo';
+	elseif (city = 'Aguada' or city = 'Aguadilla' or city = 'Anasco' or city = 'Cabo Rojo' or city = 'Hormigueros' or city = 'Isabela' or 
+		city = 'Las Marias' or city = 'Mayaguez' or city = 'Moca' or city = 'Rincon' or city = 'San German' or city = 'San Sebastian') then
+		return 'Mayaguez';
+	elseif (city = 'Adjuntas' or city = 'Guanica' or city = 'Guayanilla' or city = 'Jayuya' or city = ' Juana Diaz' or city = 'Lajas' or 
+		city = 'Lares' or city = 'Maricao' or city = 'Penuelas' or city = 'Ponce' or city = 'Sabana Grande' or city = 'Utuado' or city = 'Yauco') then
+		return 'Ponce';
+	elseif (city = 'Aibonito' or city = 'Arroyo' or city = 'Barranquitas' or city = 'Cayey' or city = 'Cidra' or city = 'Coamo' or 
+		city = 'Comerio' or city = 'Corozal' or city = 'Guayama' or city = 'Naranjito' or city = 'Orocovis' or 
+		city = 'Salinas' or city = 'Santa Isabel' or city = 'Villalba') then
+		return 'Guayama';
+	elseif (city = 'Caguas' or city = 'Gurabo' or city = 'Humacao' or city = 'Juncos' or city = 'Las Piedras' or city = 'Maunabo' or 
+		city = 'Naguabo' or city = 'Patillas' or city = 'San Lorenzo' or city = 'Yabucoa') then
+		return 'Humacao';
+	elseif (city = 'Canovanas' or city = 'Carolina' or city = 'Ceiba' or city = 'Culebra' or city = 'Fajardo' or 
+		city = 'Loiza' or city = 'Luquillo' or city = 'Rio Grande' or city = 'Trujillo Alto' or city = 'Vieques') then
+		return 'Carolina';
+	end if;
+ end
+$$ language plpgsql;
--- stat_queries.sql
+++ stat_queries.sql
@@ -37,3 +37,32 @@ from resources natural inner join (supplied  natural full join requests)
 where rdate_added >= '24-12-2017' and rdate_added <= '30-12-2017'
 group by rcategory
 order by rcategory
+
+--Regional available count
+with supplied as (select sid, rid, coalesce(sprice, 0) as sprice 
+from donates natural full join supplies),
+owner_info as (select uid, sid, city from supplier natural inner join (appuser natural inner join address))
+select get_district(city) as district, coalesce(sum(rquantity), 0) as available, coalesce(sum(case when sprice = 0 then rquantity end), 0) as donations, coalesce(sum(case when sprice != 0 then rquantity end), 0) as supply
+from resources natural inner join (supplied natural inner join owner_info)
+group by district
+order by district
+
+
+--Regional request count
+with owner_info as (select uid, sid, city from supplier natural inner join (appuser natural inner join address))
+select get_district(city) as district, sum(rquantity) as requests 
+from resources natural inner join (requests natural inner join owner_info)
+group by district
+order by district
+
+--Quantity for each category in region
+with supplied as (select sid, rid, coalesce(sprice, 0) as sprice 
+from donates natural full join supplies),
+owner_info as (select uid, sid, city from supplier natural inner join (appuser natural inner join address))
+select get_district(city) as district, rcategory, coalesce(sum(case when sprice is null then rquantity end),0) as request, 
+coalesce(sum(case when sprice >=0 then rquantity end), 0) as available
+from resources natural inner join ((supplied  natural full join requests) natural inner join owner_info)
+group by district, rcategory
+order by district, rcategory
+
+
